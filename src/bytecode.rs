e20f0d452b9e02797befa94fcf9d1bb043f1cdd0
imxyy_soope_ <3516554684@qq.com>
2024-05-03 23:00:28 +0800

    feat: stash

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,163 +1,122 @@
-use std::ops::{AddAssign, SubAssign};
+use std::hash::{Hasher, Hash};
 
-pub struct ByteCode {
-    pub(crate) inner: Box<[u8]>,
-    pub(crate) pos: usize,
+use anyhow::Error;
+
+pub type ThunkIdx = usize;
+pub type ConstIdx = usize;
+pub type Symbol = usize;
+
+#[derive(Debug, Clone)]
+pub enum Const {
+    Int(i64),
+    Float(f64),
+    String(String),
 }
 
-/* impl Debug for ByteCode {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut result = String::new();
-        let header = "         00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F".cyan();
-        result.push_str(&header);
-        result.push('\n');
+impl<'a> TryFrom<&'a Const> for &'a i64 {
+    type Error = Box<dyn Error>;
 
-        let current_pos = self.pos;
-        let mut replica = ByteCode {
-            inner: self.inner,
-            pos: self.pos,
-        };
-        replica.reset();
-
-        // If the amount of data is large, it may be better to use a lookup table.
-        let mut content: Vec<String> = replica
-            .inner
-            .iter()
-            .map(|byte| format!("{:02X}", byte))
-            .collect();
-        if let Some(byte) = content.get_mut(current_pos) {
-            *byte = byte.green();
+    fn try_from(value: &'a Const) -> Result<Self, Self::Error> {
+        match value {
+            Const::Int(int) => Ok(int),
+            _ => panic!(),
         }
-        for (i, line) in content.chunks(16).map(|line| line.join(" ")).enumerate() {
-            let line_number = {
-                let mut line_number = "0".repeat(8);
-                let hex = format!("{:02X}", i);
-                line_number.replace_range((8 - hex.len() - 1)..7, &hex);
-                line_number
-            };
-            result.push_str(&line_number);
-            result.push(' ');
-            result.push_str(&line);
-            result.push('\n');
-        }
-        write!(f, "\n{}", result)
-    }
-} */
-
-impl ByteCode {
-    pub fn new(slice: Box<[u8]>) -> Self {
-        ByteCode {
-            inner: slice,
-            pos: 0,
-        }
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.inner[self.pos..self.len()]
-    }
-
-    pub fn len(&self) -> usize {
-        self.inner.len()
-    }
-
-    pub fn pos(&self) -> usize {
-        self.pos
-    }
-
-    pub fn reset(&mut self) {
-        self.pos = 0;
-    }
-
-    pub fn is_end(&self) -> bool {
-        self.pos == self.len()
-    }
-
-    pub fn peek(&self, num: usize) -> &[u8] {
-        if num > self.len() {
-            panic!(
-                "range end index {} out of range for slice of length {}",
-                num,
-                self.len()
-            );
-        }
-        &self.inner[self.pos..self.pos + num]
-    }
-
-    pub fn starts_with(&self, v: &[u8]) -> bool {
-        self.peek(v.len()) == v
-    }
-
-    pub fn next(&mut self) {
-        *self += 1;
-    }
-
-    pub fn prev(&mut self) {
-        *self -= 1;
-    }
-
-    pub fn skip(&mut self, num: usize) {
-        *self += num;
-    }
-
-    pub fn take(&mut self, num: usize) -> &[u8] {
-        if num > self.len() {
-            panic!(
-                "index out of bounds: the slice only lengths {}, but tried to take {}",
-                self.len(),
-                num
-            )
-        }
-        let result = &self.inner[self.pos..self.pos + num];
-        self.pos += num;
-        result
-    }
-
-    pub fn take_u8(&mut self) -> u8 {
-        self.take(1)[0]
-    }
-
-    pub fn take_u16(&mut self) -> u16 {
-        let bytes: [u8; 2] = self.take(2).try_into().unwrap();
-        u16::from_le_bytes(bytes)
-    }
-
-    pub fn take_u32(&mut self) -> u32 {
-        let bytes: [u8; 4] = self.take(4).try_into().unwrap();
-        u32::from_le_bytes(bytes)
-    }
-
-    pub fn take_u64(&mut self) -> u64 {
-        let bytes: [u8; 8] = self.take(8).try_into().unwrap();
-        u64::from_le_bytes(bytes)
-    }
-
-    pub fn take_into_string(&mut self, num: usize) -> String {
-        let bytes = self.take(num);
-        String::from_utf8(bytes.to_vec()).unwrap()
     }
 }
 
-impl AddAssign<usize> for ByteCode {
-    fn add_assign(&mut self, rhs: usize) {
-        if rhs > self.inner.len() {
-            panic!(
-                "index out of bounds: the slice can only move forward {}, but tried to move {}",
-                self.inner.len(),
-                rhs
-            );
+impl<'a> TryFrom<&'a Const> for &'a f64 {
+    type Error = Box<dyn Error>;
+
+    fn try_from(value: &'a Const) -> Result<Self, Self::Error> {
+        match value {
+            Const::Float(float) => Ok(float),
+            _ => panic!(),
         }
-        self.pos += rhs;
     }
 }
 
-impl SubAssign<usize> for ByteCode {
-    fn sub_assign(&mut self, rhs: usize) {
-        if rhs > self.pos {
-            panic!(
-                "index out of bounds: the slice can only move back {}, but tried to move {}",
-                self.pos, rhs
-            );
+impl<'a> TryFrom<&'a Const> for &'a str {
+    type Error = Box<dyn Error>;
+
+    fn try_from(value: &'a Const) -> Result<Self, Self::Error> {
+        match value {
+            Const::String(string) => Ok(string),
+            _ => panic!(),
         }
-        self.pos -= rhs;
     }
 }
+
+impl PartialEq for Const {
+    fn eq(&self, other: &Self) -> bool {
+        use Const::*;
+        match self {
+            Int(int) => other.try_into().map_or(false, |other| int.eq(other)),
+            Float(float) => other.try_into().map_or(false, |other: &f64| float.to_bits().eq(&other.to_bits())),
+            String(string) => other.try_into().map_or(false, |other| string.eq(other))
+        }
+    }
+}
+
+impl Eq for Const {}
+
+impl Hash for Const {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        use Const::*;
+        match *self {
+            Int(int) => int.hash(state),
+            Float(float) => float.to_bits().hash(state),
+            String(ref string) => string.hash(state),
+        }
+    }
+}
+
+pub enum OpCode {
+    /// load a constant onto stack
+    Const { idx: ConstIdx },
+    /// load a thunk lazily onto stack
+    Thunk { idx: ThunkIdx },
+    /// load a thunk onto stack and force its value
+    ThunkValue { idx: ThunkIdx },
+    /// force TOS to value
+    ForceValue,
+    /// call a funcion thunk with `arity` numbers of parameters on the stack
+    CallThunk { thunk: ThunkIdx, arity: usize },
+    /// assert TOS is true
+    Assert,
+    /// if TOS is true, then load `consq` onto stack, else load `alter` onto stack
+    IfElse { consq: ThunkIdx, alter: ThunkIdx },
+    /// push an empty attribute set onto stack
+    AttrSet,
+    /// push an static attribute into the attribute set at TOS
+    PushStaticAttr { name: ConstIdx, value: ThunkIdx },
+    /// push an dynamic attribute into the attribute set at TOS
+    PushDynAttr { name: ThunkIdx, value: ThunkIdx },
+    /// push an empty list onto stack
+    List,
+    /// push an element into the list at TOS
+    PushElem { elem: ThunkIdx },
+    /// perform a binary operation
+    BinOp { op: BinOp },
+    /// perform a unary operation
+    UnOp { op: UnOp },
+    /// check whether the attribute set at TOS has an specific attribute
+    // HasAttr { attr: Const }
+    /// enter the environment of the attribute set at TOS
+    EnterEnv,
+    /// exit the envrironment
+    ExitEnv,
+}
+
+pub enum BinOp {
+    Add,
+    Mul,
+    Div,
+    And,
+    Or,
+}
+
+pub enum UnOp {
+    Neg,
+    Not,
+}
+
--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1,1 +1,13 @@
+use crate::bytecode::*;
 
+pub struct VM {
+    code: Box<dyn Iterator<Item = OpCode>>,
+}
+
+impl VM {
+    pub fn new(code: Into<dyn Iterator>) -> VM {
+        VM {
+            code: code.into()
+        }
+    }
+}
