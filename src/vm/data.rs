0c0eeb9b54f94b3f5bb025dc15dd1917ae8a28e1
imxyy_soope_ <3516554684@qq.com>
2024-06-21 21:49:52 +0800

    feat: stash

--- src/vm/data.rs
+++ src/vm/data.rs
@@ -1,9 +1,9 @@
 use std::sync::RwLock;
 
-use derive_more::IsVariant;
+use derive_more::{IsVariant, Unwrap};
 use anyhow::{Result, anyhow};
 
-use crate::bytecode::{OpCodes, ThunkIdx, Thunks, Thunk};
+use crate::bytecode::OpCodes;
 
 use super::value::*;
 use super::vm::VM;
@@ -16,7 +16,7 @@ pub enum StackElem<'vm> {
 
 pub struct VmThunk(RwLock<_VmThunk>);
 
-#[derive(IsVariant)]
+#[derive(IsVariant, Unwrap)]
 enum _VmThunk {
     Code(OpCodes),
     SuspendedFrom(*const VmThunk),
@@ -29,14 +29,13 @@ impl VmThunk {
     }
 
     pub fn force(&mut self, vm: &VM) -> Result<Value> {
-        if let _VmThunk::Value(ref value) = &*self.0.read().unwrap() {
-            return Ok(value.clone())
+        match &*self.0.read().unwrap() {
+            _VmThunk::Value(value) => return Ok(value.clone()),
+            _VmThunk::SuspendedFrom(from) => return Err(anyhow!("already suspended from {from:p} (infinite recursion encountered)")),
+            _VmThunk::Code(_) => ()
         }
         let guard = &mut *self.0.write().unwrap();
-        #[allow(unused)]
-        let _VmThunk::Code(opcodes) = std::mem::replace(&mut *self.0.write().unwrap(), _VmThunk::SuspendedFrom(self as *const VmThunk)) else {
-            return Err(anyhow!("infinite recursion occured"))
-        };
+        let opcodes = std::mem::replace(&mut *self.0.write().unwrap(), _VmThunk::SuspendedFrom(self as *const VmThunk)).unwrap_code();
         let value = vm.eval(opcodes).unwrap();
         let _ = std::mem::replace(guard, _VmThunk::Value(value));
         if let _VmThunk::Value(value) = guard {
--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -1,8 +1,7 @@
-use std::mem::MaybeUninit;
 use std::collections::HashMap;
-use std::sync::{Arc, Weak, RwLock};
-use std::cell::RefCell;
+use std::sync::Arc;
 
+use rayon::{ThreadPool, ThreadPoolBuilder};
 use anyhow::{anyhow, Result};
 
 use crate::bytecode::*;
@@ -12,9 +11,9 @@ use crate::slice::*;
 use super::data::*;
 use super::value::*;
 
-pub fn run(prog: Program) -> Result<OwnedValue> {
+pub async fn run(prog: Program) -> Result<OwnedValue> {
     let vm = VM::new(prog.consts, prog.symbols, prog.thunks);
-    Ok(vm.eval(prog.top_level)?.to_owned_value(&vm))
+    Ok(vm.eval(prog.top_level).await?.to_owned_value(&vm))
 }
 
 pub struct VM {
@@ -22,7 +21,8 @@ pub struct VM {
     thunks: Slice<Arc<VmThunk>>,
     symbols: Symbols,
     symbols_map: HashMap<String, SymIdx>,
-    dynamic_symbols: Vec<String>
+    dynamic_symbols: Vec<String>,
+    pool: ThreadPool
 }
 
 
@@ -30,16 +30,21 @@ impl VM {
     fn new(consts: Consts, symbols: Symbols, thunks: Thunks) -> Self {
         let symbols_map = symbols.clone().into_iter().enumerate().map(|(idx, sym)| (sym, idx)).collect();
         let thunks = thunks.into_iter().map(|ByteCodeThunk { opcodes }| Arc::new(VmThunk::new(opcodes))).collect();
+        let pool = ThreadPoolBuilder::new().num_threads(num_cpus::get()).spawn_handler(|thread| {
+            tokio::task::spawn_blocking(|| thread.run());
+            Ok(())
+        }).build().unwrap();
         VM {
             consts,
             thunks,
             symbols,
             symbols_map,
-            dynamic_symbols: Vec::new()
+            dynamic_symbols: Vec::new(),
+            pool
         }
     }
 
-    pub fn eval(&self, opcodes: OpCodes) -> Result<Value> {
-        todo!()
+    pub async fn eval(&self, opcodes: OpCodes) -> Result<Value> {
+
     }
 }
